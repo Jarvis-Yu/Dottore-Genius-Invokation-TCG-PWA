63ea5616e3e0a84dfe72c99c8d336e075b081f34
JarvisYu <JarvisYu_Pro@outlook.com>
2023-08-20 22:12:56 +0100

    adds sw.js attempting to make website able to be run off-line

---
+++ assets/sw.js
@@ -0,0 +1,11 @@
+var GHPATH = '/Dottore-Genius-Invokation-TCG-PWA';
+
+var APP_PREFIX = 'dgisim_';
+
+var VERSION = 'version_00';
+
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/game`,
+  `${GHPATH}/deck`,
+]
