48dbf57e3149d077052e2b4e3d3c60b84b50383d
JarvisYu <JarvisYu_Pro@outlook.com>
2023-11-15 12:57:44 +0000

    adds license for each file

--- src/game_data.py
+++ src/game_data.py
@@ -1,3 +1,19 @@
+"""
+This file is part of Dottore Genius Invokation PWA.
+
+Dottore Genius Invokation PWA is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+Dottore Genius Invokation PWA is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+Dottore Genius Invokation PWA. If not, see <https://www.gnu.org/licenses/>
+"""
 from __future__ import annotations
 from dataclasses import dataclass, field
 from typing import Callable, Literal
